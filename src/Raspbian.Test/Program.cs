036c221ddb29a96226806b045f5227678e2aa1c5
MERKATOR <MERKATOR@DESKTOP-G8UCIOV>
2019-12-26 06:35:18 +0300

    Added Raspbian.Test;

---
+++ src/Raspbian.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Raspbian.Test
+{
+	class Program
+	{
+		static void Main(String[] args)
+		{
+			Console.WriteLine("Hello world!");
+#if DEBUG
+			WaitForDebuggerAttach();
+#endif
+			Console.WriteLine("Program body");
+		}
+
+		// FUNCTIONS ////////////////////////////////////////////////////////////////////////////////////
+		private static void WaitForDebuggerAttach()
+		{
+			Console.WriteLine("waiting for debugger attach");
+			while(!Debugger.IsAttached)
+			{
+				Thread.Sleep(100);
+			}
+
+			Console.WriteLine("Debugger attached");
+		}
+	}
+}
