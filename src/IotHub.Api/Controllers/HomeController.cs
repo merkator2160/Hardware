c0be4d0db8499d14b2e3acb0fa58691795a8d615
Evgeniy Aleksandrov <2160@inbox.ru>
2021-01-23 13:28:45 +0300

    Added IotHub.Api;

---
+++ src/IotHub.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DenverTraffic.FingerprintBuilder.Api.Controllers
+{
+	[ApiController]
+#if !DEBUG
+	[ApiExplorerSettings(IgnoreApi = true)]
+#endif
+	public class HomeController : ControllerBase
+	{
+		[HttpGet("/")]
+		public IActionResult Index()
+		{
+			return Ok("Fingerprint builder service");
+		}
+	}
+}
---
+++ src/IotHub.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IotHub.Contracts
+{
+	public class Class1
+	{
+	}
+}
