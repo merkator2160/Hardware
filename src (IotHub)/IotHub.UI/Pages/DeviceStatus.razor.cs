ff563335ebfde43347083c4451f212f205c361c8
Evgeniy Aleksandrov <2160@inbox.ru>
2022-03-11 19:19:07 +0300

    Added UI code files;

---
+++ src (IotHub)/IotHub.UI/Pages/DeviceStatus.razor.cs
@@ -0,0 +1,27 @@
+﻿using IotHub.Contracts.Models.Api.DeviceMonitor;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace IotHub.UI.Pages
+{
+    [Route("deviceStatus")]
+    public partial class DeviceStatus
+    {
+        private TrackingDeviceAm[]? _unavailableDevices;
+
+
+        // PROPERTIES /////////////////////////////////////////////////////////////////////////////
+        [Inject]
+        public HttpClient Client { get; set; }
+
+
+        // FUNCTIONS //////////////////////////////////////////////////////////////////////////////
+
+        protected override async Task OnInitializedAsync()
+        {
+            _unavailableDevices = await Client.GetFromJsonAsync<TrackingDeviceAm[]>("api/DeviceMonitor/GetUnavailableDeviceInfo");
+        }
+    }
+}
---
+++ src (IotHub)/IotHub.UI/Pages/FetchData.razor.cs
@@ -0,0 +1,26 @@
+﻿using IotHub.Contracts.Models.Api.Other;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace IotHub.UI.Pages
+{
+    [Route("fetchdata")]
+    public partial class FetchData
+    {
+        private WeatherForecastAm[]? _forecasts;
+
+
+        // PROPERTIES /////////////////////////////////////////////////////////////////////////////
+        [Inject]
+        public HttpClient Client { get; set; }
+
+
+        // FUNCTIONS //////////////////////////////////////////////////////////////////////////////
+        protected override async Task OnInitializedAsync()
+        {
+            _forecasts = await Client.GetFromJsonAsync<WeatherForecastAm[]>("api/Debug/GetWeatherForecast");
+        }
+    }
+}
